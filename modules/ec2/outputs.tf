752928c29339164c8bde061af5e24e2b41f473a8
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-21 13:56:49 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,6 +1,18 @@
 # returns entire ec2_instance object which contains private_ip, public_ip, instance_id ... etc
-output "ec2_instance" {
-  value = aws_instance.ec2_instance
+#output "ec2_instance" {
+#  value = aws_instance.ec2_instance
+#}
+
+output "network" {
+  value = {
+    public_ip  = aws_instance.ec2_instance.public_ip
+    private_ip = aws_instance.ec2_instance.private_ip
+    subnet_id  = aws_instance.ec2_instance.subnet_id
+  }
+}
+
+output "security_group_ids" {
+  value = aws_instance.ec2_instance.vpc_security_group_ids
 }
 
 output "instance_id" {
