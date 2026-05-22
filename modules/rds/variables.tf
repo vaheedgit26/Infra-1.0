90208979de6bf226789a906795eb8262e9912067
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 19:07:27 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -3,17 +3,17 @@ variable "database_subnet_ids"  {}
 variable "common_tags" { default = {} }
 
 # RDS variables
-variable "identifier" {}
-variable "availability_zone" {}
-variable "engine" {}
-variable "engine_version" {}
-variable "instance_class" {}
-variable "allocated_storage" {}
-variable "storage_type" {}
-variable "storage_encrypted" {}
-variable "db_name" {}
-variable "db_username" {}
-variable "db_password" {}
+variable "identifier"             {}
+variable "availability_zone"      {}
+variable "engine"                 {}
+variable "engine_version"         {}
+variable "instance_class"         {}
+variable "allocated_storage"      {}
+variable "storage_type"           {}
+variable "storage_encrypted"      {}
+variable "db_name"                {}
+variable "db_username"            {}
+variable "db_password"            {}
 variable "vpc_security_group_ids" {}
 
 
