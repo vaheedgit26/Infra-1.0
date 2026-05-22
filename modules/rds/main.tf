cddda84d74281cc8013fe4c11bbe691f27d9b9ed
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 18:49:10 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -11,3 +11,32 @@ resource "aws_db_subnet_group" "database_subnet_group" {
     }
   )
 }
+
+# create the rds instance
+resource "aws_db_instance" "db_instance" {
+  identifier              = var.identifier
+  availability_zone       = var.availability_zone
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  instance_class          = var.instance_class
+  allocated_storage       = var.allocated_storage
+  storage_type            = "gp2"
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name  # var.db_subnet_group_name
+  vpc_security_group_ids  = var.vpc_security_group_ids
+  multi_az                = var.env == "prod" ? true : false
+  skip_final_snapshot     = var.env == "prod" ? false : true
+  backup_retention_period = var.env == "prod" ? 7 : 0
+  storage_encrypted       = true
+  deletion_protection     = var.env == "prod" ? true : false
+  publicly_accessible     = false
+  
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${local.resource_name}-${var.identifier}"
+    }
+  )
+}
