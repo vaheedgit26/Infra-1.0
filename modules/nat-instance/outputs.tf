8a55a51a3ec8d8eafb87cfa5336f7df792090759
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-22 08:53:32 +0530

    Update outputs.tf

--- modules/nat-instance/outputs.tf
+++ modules/nat-instance/outputs.tf
@@ -2,6 +2,10 @@ output "nat_instance_id" {
   value = module.ec2.instance_id
 }
 
+output "nat_instance_public_ip" {
+  value = module.ec2.network.public_ip
+}
+
 output "primary_network_interface_id" {
   value = module.ec2.primary_network_interface_id
 }
