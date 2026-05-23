8028a3a88534bbbee6ccd0aea717c63f1260463f
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 11:54:00 +0530

    Update outputs.tf

--- modules/secrets-manager/outputs.tf
+++ modules/secrets-manager/outputs.tf
@@ -1,3 +1,8 @@
+output "db_secret_name" {
+  description = "Name of the secret created"
+  value       = aws_secretsmanager_secret.db_credentials.name
+}
+
 output "db_secret_arn" {
   description = "ARN of the database credentials secret"
   value       = aws_secretsmanager_secret.db_credentials.arn
