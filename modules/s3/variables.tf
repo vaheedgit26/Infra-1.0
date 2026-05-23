3b03ae09cbc756a9c28514c06c5ff9c1f4ac1e37
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 13:45:42 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,4 +2,8 @@ variable "s3_bucket_name" {}
 variable "project" {}
 variable "env" {}
 variable "region" {}
-variable "common_tags" { default = {} }
+
+variable "common_tags" { 
+  type    = map(string)
+  default = {} 
+}
