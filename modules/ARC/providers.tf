d158bef1d242fa6232b636c05930fbcd8f2e4686
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-23 11:05:47 +0530

    Create providers.tf

---
+++ modules/ARC/providers.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
