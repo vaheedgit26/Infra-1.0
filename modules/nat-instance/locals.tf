df50ea68745604d5ed20c954728cb28d49eae9f8
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-31 08:56:13 +0530

    Update locals.tf

--- modules/nat-instance/locals.tf
+++ modules/nat-instance/locals.tf
@@ -1,7 +1,7 @@
 locals {
   resource_name = "${var.project}-${var.env}"
 
-  nat-instance-sg      = "${var.resource_name}-nat-instance-sg"
-  private-instance-sg  = "${var.resource_name}-private-instance-sg"
-  database-instance-sg = "${var.resource_name}-database-instance-sg"
+  nat-instance-sg      = "${local.resource_name}-nat-instance-sg"
+  private-instance-sg  = "${local.resource_name}-private-instance-sg"
+  database-instance-sg = "${local.resource_name}-database-instance-sg"
 }
