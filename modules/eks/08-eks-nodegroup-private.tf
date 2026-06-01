2838982f588b642425ccd4acaf232f7d18f388aa
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-01 20:29:18 +0530

    Update 08-eks-nodegroup-private.tf

--- modules/eks/08-eks-nodegroup-private.tf
+++ modules/eks/08-eks-nodegroup-private.tf
@@ -36,7 +36,8 @@ resource "aws_eks_node_group" "main" {
   depends_on = [
     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_AmazonSSMManagedInstanceCore
   ]
 
   tags = merge(
