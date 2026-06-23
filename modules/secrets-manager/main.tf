2342d0858ee8dc74db03bba2e6526ef4300c00e4
vaheedgit26 <vaheed.aws@gmail.com>
2026-06-23 12:06:44 +0530

    Update main.tf

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -14,7 +14,8 @@ resource "aws_secretsmanager_secret" "db_credentials" {
 resource "aws_secretsmanager_secret_version" "db_credentials" {
   secret_id = aws_secretsmanager_secret.db_credentials.id
   secret_string = jsonencode({
-    username = var.db_username
-    password = var.db_password
+    username    = var.db_username
+    password    = var.db_password
+    jwt_secret  = var.jwt_secret
   })
 }
